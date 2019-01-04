39a46fae58a2aa20a5f2d9fa2a9d9199cf8cefaf
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 20:51:01 +0700

    Add files via upload

---
+++ Konversi Detik ke Hari, Jam, Menit.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+
+
+float detik,hd,jd,md ;
+
+printf("Masukkan Hari : "); scanf("%i",&hd);
+detik=hd*24*60*60;
+printf("Hasilnya adalah = %i\n",detik);
+printf("Masukkan jam : "); scanf("%i",&jd);
+detik=jd*60*60
+printf("Satu hari adalah = %i\n",detik);
+getch();
+}
+
+
